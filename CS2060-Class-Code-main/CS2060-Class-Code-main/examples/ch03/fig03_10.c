b7b512498b88533d2a7dbaa8741513538c0176c9
MordredCameron <mcameronstimmt@gmail.com>
2023-01-31 09:14:00 -0700

    Update fig03_10.c

--- CS2060-Class-Code-main/CS2060-Class-Code-main/examples/ch03/fig03_10.c
+++ CS2060-Class-Code-main/CS2060-Class-Code-main/examples/ch03/fig03_10.c
@@ -1,6 +1,7 @@
 // Fig. 3.10: fig03_10.c
 // Analysis of examination results 
 #include <stdio.h>
+#include <stdbool.h>
 
 // function main begins program execution 
 int main( void )
@@ -9,24 +10,45 @@ int main( void )
    unsigned int passes = 0; // number of passes   
    unsigned int failures = 0; // number of failures 
    unsigned int student = 1; // student counter    
-   int result; // one exam result 
+   int result = 0; // one exam result 
+   bool valid = false;
 
    // process 10 students using counter-controlled loop 
    while ( student <= 10 ) {
 
       // prompt user for input and obtain value from user 
       printf( "%s", "Enter result ( 1=pass,2=fail ): " );
-      scanf( "%d", &result );
 
-      // if result 1, increment passes 
-      if ( result == 1 ) {     
-         passes = passes + 1;
-      } // end if 
-      else { // otherwise, increment failures 
-         failures = failures + 1;
-      } // end else 
+      //check for valid input
+      do {
+          int scanfReturn = scanf("%d", &result);
+          //clear buffer
+          while ((getchar()) != '\n');
 
-      student = student + 1; // increment student counter  
+          //check for int
+          if (scanfReturn == 1) {
+              //check in range
+              if (result >= 1 && result <= 2) {
+                  // if result 1, increment passes 
+                  if (result == 1) {
+                      passes = passes + 1;
+                  } // end if 
+                  else { // otherwise, increment failures 
+                      failures = failures + 1;
+                  } // end else 
+
+                  student = student + 1; // increment student counter 
+                  valid = true;
+              }
+              else {
+                  puts("Please input 1 or 2");
+              }
+          }
+          else {
+              puts("Please enter a number");
+          }
+      } while(valid == false);
+
    } // end while 
 
    // termination phase; display number of passes and failures 
