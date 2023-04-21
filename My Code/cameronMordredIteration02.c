072828d9c396679041375733367c17f288b350f5
MordredCameron <mcameronstimmt@gmail.com>
2023-04-21 08:20:37 -0600

    Create cameronMordredIteration02.c

---
+++ My Code/cameronMordredIteration02.c
@@ -0,0 +1,536 @@
+//Iteration one - Set up organization, prompt for donations, and display results after logging in 
+#include <stdio.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+
+//constants
+#define CHOICE_SIZE 2
+#define BUFF_LENGTH 80
+#define FEE 0.031
+#define FOLDER_PATH	"C:\\Users\\Mordred\\Documents\\GitHubRepos\\CS2060\\Files\\fundraiser\\"
+
+//Structures
+typedef struct organization {
+	char organizationName[BUFF_LENGTH];
+	char name[BUFF_LENGTH];
+	char emailAddress[BUFF_LENGTH];
+	char purpose[BUFF_LENGTH];
+	char goalAmount[BUFF_LENGTH];
+	char password[BUFF_LENGTH];
+	char url[BUFF_LENGTH];
+	char urlFull[BUFF_LENGTH]; 
+	double donations;
+	int numDonations;
+	double creditCard;
+	struct organization* next; 
+}Organization;
+
+typedef struct donation {
+	char amount[BUFF_LENGTH];
+	char name[BUFF_LENGTH];
+	char zip[BUFF_LENGTH];
+	double totalDonation;
+}Donation;
+
+//functions
+bool correctYN();
+void setUpOrganization(Organization* orgPtr);
+void displayOrganization(Organization* orgPtr);
+void insert(Organization** headPtr);
+int compare(char* name, char* input);
+void fGets(char* input);
+bool validInt(char* input);
+void createURL(Organization* orgPtr); 
+void createFile(Organization* orgPtr);
+void setUpDonation(Donation* donPtr, Organization* orgPtr);
+void selectOrg(Organization** headPtr, Donation* donPtr);
+void validZip(Donation* donPtr);
+void reciept(char* orgName, double donTotal);
+void cardFee(Donation* donPtr, Organization* orgPtr);
+void login(Donation* donPtr, Organization* orgPtr);
+void displayFundraiser(Organization* orgPtr); 
+void printFundraisers(Organization* headPtr); 
+
+
+int main(void) {
+	//create organization structure
+	Organization* orgHeadPtr = NULL;
+	//initialize variables to zero
+	//create donation structure
+	Donation don;
+	Donation* donPtr = &don; 
+	bool cont = true;
+	bool orgCont = false; 
+	//bool delCont = false;
+	//insert pets
+	while (cont) {
+		insert(&orgHeadPtr);
+		puts("Do you want to enter another organization? y/n");
+		cont = correctYN();
+	}
+	printFundraisers(orgHeadPtr); 
+	//displayFundraiser(org1Ptr); 
+	while (!orgCont) {
+		puts("Enter the name of the organization you want to register.");
+		selectOrg(&orgHeadPtr, donPtr);
+	}
+	//setUpDonation(don1Ptr, org1Ptr);
+
+	return 0; 
+}
+//insert into linked list
+void insert(Organization** headPtr) {
+
+	Organization* newOrgPtr = (Organization*)malloc(sizeof(Organization));
+	//prompt data
+	setUpOrganization(newOrgPtr);
+	newOrgPtr->next = NULL;
+
+	//head or greater
+	if (*headPtr == NULL || (compare((*headPtr)->organizationName, newOrgPtr->organizationName) >= 0)) {
+		newOrgPtr->next = *headPtr;
+		*headPtr = newOrgPtr;
+		//make it stop aaa
+		return;
+	}
+
+	//find place to insert
+	Organization* current = *headPtr;
+	while (current->next != NULL && (compare(current->organizationName, newOrgPtr->organizationName) <= 0)) {
+		current = current->next;
+	}
+	//move on yall
+	newOrgPtr->next = current->next;
+	current->next = newOrgPtr;
+
+	printf("\nThank you %s. The url to raise funds for %s is %s\n", newOrgPtr->name, newOrgPtr->organizationName, newOrgPtr->url);
+}
+//compare strings 
+int compare(char* name, char* input) {
+	char a[BUFF_LENGTH];
+	strcpy(a, name);
+	//make it all lower case 
+	for (int i = 0; i < strlen(a); i++)
+	{
+		a[i] = tolower(a[i]);
+	}
+	for (int i = 0; i < strlen(input); i++)
+	{
+		input[i] = tolower(input[i]);
+	}
+	int result = strcmp(a, input);
+	return result;
+}
+bool correctYN() {
+	char yesNo[BUFF_LENGTH];
+	bool correct = false;
+	bool cont = false;
+	//cont till yes or no 
+	while (!cont) {
+		fGets(yesNo);
+		//if user enters y/Y then proceed to next step 
+		if (strcmp(yesNo, "y") == 0 || strcmp(yesNo, "Y") == 0) {
+			correct = true;
+			cont = true;
+		}
+		else if (strcmp(yesNo, "n") == 0 || strcmp(yesNo, "N") == 0) {
+			correct = false;
+			cont = true;
+		}
+		//continues until either y or n is entered 
+		else {
+			puts("Please enter (y)es y/Y or (n)o n/N");
+		}
+	}
+	return correct;
+}
+//prompt organization questions 
+void setUpOrganization(Organization* orgPtr) {
+	bool cont = false;
+	//fill in organization info
+	puts("Enter organization name");
+	fgets(orgPtr->organizationName, BUFF_LENGTH, stdin);
+	puts("Enter your name");
+	fgets(orgPtr->name, BUFF_LENGTH, stdin);
+	puts("Enter purpose");
+	fgets(orgPtr->purpose, BUFF_LENGTH, stdin);
+	puts("Enter goal amount");
+	orgPtr->donations = 0; 
+	bool contInt = false;
+	//loop till valid int
+	while (!contInt) {
+		fGets(orgPtr->goalAmount);
+		contInt = validInt(orgPtr->goalAmount);
+	}
+	//continue loop while email is incorrect
+	while (!cont) {
+		puts("Enter email");
+		fgets(orgPtr->emailAddress, BUFF_LENGTH, stdin);
+		//bool correct = correctEmail();
+		puts("\nIs this email correct? (y)es y/Y or (n)o n/N");
+		bool correct = correctYN(); 
+		//create password only if correctEmail function returns true 
+		if (correct == true) {
+			puts("Create Password");
+			fgets(orgPtr->password, BUFF_LENGTH, stdin);
+			cont = true;
+		}
+		//loop until valid email 
+		else {
+			cont = false;
+		}
+	}
+	createURL(orgPtr);
+	//createFile(orgPtr); 
+}
+void fGets(char* input) {
+	fgets(input, BUFF_LENGTH, stdin);
+	//remove ending
+	size_t inputLength = strnlen(input, BUFF_LENGTH);
+	if (inputLength > 0 && input[inputLength - 1] == '\n')
+	{
+		input[inputLength - 1] = '\0';
+		inputLength--;
+	}
+}
+
+
+//validate goal amount 
+bool validInt(char* input) {
+	
+	bool cont = false;
+		//test for valid integer
+		char* end;
+		errno = 0;
+		//int validInt = 0;
+		double intTest = strtod(input, &end);
+		//if the string matches the ending character
+		if (end == input) {
+			fprintf(stderr, "%s: not an integer\n", input);
+		}
+		//if it is not the null character at the end of the converted string, there are extra characters 
+		else if ('\0' != *end) {
+			fprintf(stderr, "%s: extra characters at end of input: %s\n", input, end);
+		}
+		//when it is unsuccessful and stores ERANGE in errno, check if it was caused by being out of range 
+		else if ((LONG_MIN == intTest || LONG_MAX == intTest) && ERANGE == errno) {
+			fprintf(stderr, "%s Error out of range of long int\n", input);
+		}
+		//if the integer is greater then the acceptable max
+		else if (intTest > INT_MAX) {
+			fprintf(stderr, "%f greater than INT_MAX\n", intTest);
+		}
+		//if the integer is less then the acceptable min 
+		else if (intTest < 0) {
+			fprintf(stderr, "%f less than zero\n", intTest);
+		}
+		//if there were no errors, cast intTest into an int and store in validInt
+		else {
+			//validInt = (int)intTest;
+			cont = true;
+		}
+		return cont; 
+}
+
+//create url using organization name
+void createURL(Organization* orgPtr) {
+	//create strings holding url parts
+	char front[BUFF_LENGTH] = "https:donate.com/";
+	char back[BUFF_LENGTH] = "?form=popup#";
+	//copy organization name to url
+	strcpy(orgPtr->url, orgPtr->organizationName);
+	char* strPtr = orgPtr->url;
+	//loop through url name with pointer and replace spaces with - 
+	while ((strPtr = strchr(strPtr, ' ')) != NULL) {
+		*strPtr++ = '-';
+	}
+	//combine strings
+	strcpy(orgPtr->urlFull, orgPtr->url);
+	strcat(front, orgPtr->urlFull);
+	strcat(front, back);
+	strcpy(orgPtr->urlFull, front);
+	printf("\nThank you %s. The url to raise funds for %s is %s\n", orgPtr->name, orgPtr->organizationName, orgPtr->urlFull);
+
+}
+
+void createFile(Organization* orgPtr) {
+	FILE* filePtr;
+	char name[BUFF_LENGTH];
+	char path[BUFF_LENGTH];
+	strcpy(name, orgPtr->url);
+	strcpy(path, FOLDER_PATH);
+	strcat(path, name);
+	strcat(path, ".txt");
+	filePtr = fopen(path, "w");
+	//check if you can make file
+	if (filePtr == NULL) {
+		printf("Unable to create file.\n");
+	}
+
+	//close file
+	fclose(filePtr);
+}
+
+//prompts donation questions
+void setUpDonation(Donation* donPtr, Organization* orgPtr) {
+	bool cont = false; 
+	//loop until organization enters q/Q
+	//while (!cont) {
+		displayFundraiser(orgPtr);
+		//always prompt for amount
+		puts("\nEnter donation amount");
+		fGets(donPtr->amount);
+		//if q is entered, skip regular donation and move to login 
+		if (strcmp(donPtr->amount, "q") == 0 || strcmp(donPtr->amount, "Q") == 0) {
+			//cont = true;
+			login(donPtr, orgPtr); 
+		}
+		//if regular donation, prompt for info 
+		else if (validInt(donPtr->amount) == true) {
+
+			puts("Enter your name");
+			fgets(donPtr->name, BUFF_LENGTH, stdin);
+			puts("Enter your 5 digit zip code");
+			validZip(donPtr);
+			//incriment number of donations
+			orgPtr->numDonations++;
+			cardFee(donPtr, orgPtr);
+			//ask for reciept
+			reciept(orgPtr->organizationName, donPtr->totalDonation);
+		}
+	//}
+}
+void selectOrg(Organization** headPtr, Donation* donPtr) {
+	bool cont = false; 
+	Organization* current = *headPtr;
+
+	char name[80];
+	fgets(name, BUFF_LENGTH, stdin);
+	puts((*headPtr)->organizationName);
+	while (current != NULL && (compare(current->organizationName, name) != 0) && !cont) {
+		
+			current = current->next;
+			puts("moved");
+	}
+	if (current != NULL) {
+		setUpDonation(donPtr, current);
+		cont = true; 
+	}
+	if (current == NULL) {
+		printf("%s not found", name);
+	}
+	
+}
+//calculates fee
+void cardFee(Donation* donPtr, Organization* orgPtr) {
+	char* end;
+	//convert to double
+	double donationAmount = strtod(donPtr->amount, &end);
+	//calculate card fee
+	double cardFee = donationAmount * FEE; 
+	//calculate total
+	double total = donationAmount + cardFee;
+	//send info to structure 
+	orgPtr->creditCard += cardFee;
+	donPtr->totalDonation = total; 
+	printf("\nThank you for your donation. There is a 3.1 percent credit card processing fee of %.2f. %.2f will be donated.", 
+		donationAmount, total);
+	orgPtr->donations += total; 
+}
+
+//prompts for reciept 
+void reciept(char* orgName, double donTotal) {
+	char yesNo[BUFF_LENGTH];
+	bool cont = false;
+	puts("Do you want a reciept? (y)es y/Y or (n)o n/N");
+	//continue to ask until user inputs acceptable answer 
+	while (!cont) {
+		fgets(yesNo, BUFF_LENGTH, stdin);
+		//remove extra elements
+		size_t inputLength = strnlen(yesNo, BUFF_LENGTH);
+		if (inputLength > 0 && yesNo[inputLength - 1] == '\n')
+		{
+			yesNo[inputLength - 1] = '\0';
+			inputLength--;
+		}
+		//print out reciept info while user inputs y/Y
+		if (strcmp(yesNo, "y") == 0 || strcmp(yesNo, "Y") == 0) {
+			cont = true;
+			printf("\nOrganization: %s", orgName);
+			printf("Donation Amount: %.2f", donTotal);
+			//find local time and date
+			time_t now;
+			time(&now);
+			//convert time to local time
+			struct tm* local = localtime(&now);
+			int hours = local->tm_hour;         
+			int minutes = local->tm_min;        
+			int seconds = local->tm_sec; 
+			int day = local->tm_mday;            
+			int month = local->tm_mon + 1;      
+			int year = local->tm_year + 1900;
+			//if its am
+			if (hours < 12) {    
+				printf("\nDonation Date:  %02d/%02d/%d - %02d:%02d:%02d AM\n", day, month, year, hours, minutes, seconds);
+			}
+			//if pm
+			else {    
+				printf("\nDonation Date:  %02d/%02d/%d - %02d:%02d:%02d PM\n", day, month, year, hours - 12, minutes, seconds);
+			}
+
+		}
+		//move on to next step
+		else if (strcmp(yesNo, "n") == 0 || strcmp(yesNo, "N") == 0) {
+			cont = true;
+		}
+		else {
+			puts("Please enter (y)es y/Y or (n)o n/N");
+		}
+	}
+}
+
+
+//validate zip code
+void validZip(Donation* donPtr) {
+	bool cont = false;
+	
+	//continue while incorrect input
+	while (!cont) {
+
+		fgets(donPtr->zip, BUFF_LENGTH, stdin);
+
+		//remove ending elements
+		size_t inputLength = strnlen(donPtr->zip, BUFF_LENGTH);
+		if (inputLength > 0 && donPtr->zip[inputLength - 1] == '\n')
+		{
+			donPtr->zip[inputLength - 1] = '\0';
+			inputLength--;
+		}
+		//test for valid integer
+		char* end;
+		errno = 0;
+		long intTest = strtol(donPtr->zip, &end, 10);
+		//create character to check if zip starts with 0 
+		char first;
+		first = donPtr->zip[0];
+		//if the string matches the ending character
+		if (end == donPtr->zip) {
+			fprintf(stderr, "%s: not an integer\n", donPtr->zip);
+		}
+		//if it is not the null character at the end of the converted string, there are extra characters 
+		else if ('\0' != *end) {
+			fprintf(stderr, "%s: extra characters at end of input: %s\n", donPtr->zip, end);
+		}
+		//when it is unsuccessful and stores ERANGE in errno, check if it was caused by being out of range 
+		else if ((LONG_MIN == intTest || LONG_MAX == intTest) && ERANGE == errno) {
+			fprintf(stderr, "%s Error out of range of long int\n", donPtr->zip);
+		}
+		//if the integer is greater then the acceptable max
+		else if (intTest > INT_MAX) {
+			fprintf(stderr, "%ld greater than INT_MAX\n", intTest);
+		}
+		//if the integer is less then the acceptable min 
+		else if (intTest < 0) {
+			fprintf(stderr, "%ld less than zero\n", intTest);
+		}
+		//if zip is not 5 numbers
+		else if (inputLength > 5 || inputLength < 5) {
+			puts("Invalid zip length");
+		}
+		//if zip starts with 0
+		else if (first == '0') {
+			puts("Cannot start with zero");
+		}
+		//if there were no errors, cast intTest into an int and store in validInt
+		else {
+			cont = true;
+		}
+	}
+
+}
+
+//login for organization
+void login(Donation* donPtr, Organization* orgPtr) {
+	char email[BUFF_LENGTH];
+	char password[BUFF_LENGTH];
+	bool correct = false;
+	bool correctP = false;
+	int counter = 0;
+	int counterP = 0;
+	puts("\nEnter email");
+	//loop twice to prompt email
+	while (!correct && counter != 3) {
+		fgets(email, BUFF_LENGTH, stdin);
+		//if input does not match email
+		if (strcmp(email, orgPtr->emailAddress) != 0) {
+			puts("Incorrect email");
+			counter++;
+		}
+		else {
+			puts("Enter password");
+			//loop twice to prompt password
+			while (!correctP && counterP != 3) {
+				fgets(password, BUFF_LENGTH, stdin);
+				//if input does not match password 
+				if (strcmp(password, orgPtr->password) != 0) {
+					puts("Incorrect password");
+					counterP++;
+				}
+				//move on to display information 
+				else {
+					correctP = true;
+					displayOrganization(orgPtr);
+				}
+			}
+			correct = true;
+		}
+	}
+	//go back to donations mode if incorrect email and password were entered twice
+	if (!correct || !correctP) {
+		puts("\nGoing back to donations mode");
+		setUpDonation(donPtr, orgPtr);
+	}
+}
+
+//print info to admin
+void displayOrganization(Organization* orgPtr) {
+	printf("Organization name: %s\n", orgPtr->organizationName);
+	printf("Total Number of Donations: %d\n", orgPtr->numDonations);
+	printf("Total Amount Raised: %.2f\n", orgPtr->donations);
+	printf("Total amount paid for credit card processing: %.2f\n", orgPtr->creditCard);
+}
+
+void printFundraisers(Organization* headPtr) {
+	Organization* current = headPtr;
+	//start from the beginning
+	if (current == NULL) {
+		puts("No organizations in list");
+	}
+	while (current != NULL) {
+		puts("Organization    Goal Amount    Donations");
+		printf("%s    %s    %.2f \n", current->organizationName, current->goalAmount, current->donations);
+		current = current->next;
+	}
+}
+
+void displayFundraiser(Organization* orgPtr) {
+	puts("\nMAKE A DIFFERENCE BY YOUR DONATION");
+	printf("Organization name: %s\n", orgPtr->organizationName);
+	printf("Purpose: %s\n", orgPtr->purpose);
+	printf("We have currently raised $%.2f\n", orgPtr->donations);
+	char* end;
+	//convert to double
+	double amount = strtod(orgPtr->goalAmount, &end);
+	double percent = (orgPtr->donations / amount) * 100;
+	if (orgPtr->donations >= amount) {
+		puts("We have reached our goal but can still use the donations.");
+	}
+	else {
+		printf("We are %.2f percent towards our goal of $%.2f\n", percent, amount);
+	}
+}
